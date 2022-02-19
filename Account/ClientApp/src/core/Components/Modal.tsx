bd7e9fc072d2fcbd9fcc6a3d10c06e5c9186140f
Jay Mirecki <jarett.mirecki@gmail.com>
2022-02-18 22:43:10 -0600

    Create Modal

---
+++ Account/ClientApp/src/core/Components/Modal.tsx
@@ -0,0 +1,44 @@
+import {
+    ChangeEvent,
+    FocusEvent,
+    KeyboardEvent,
+    useState
+} from 'react';
+import { Button } from '../Components';
+import '../Styles/Modal.css';
+import { IComponentProps } from './IComponentProps';
+import { XRow } from './XRow';
+
+interface IModalProps extends IComponentProps {
+    closable?: boolean;
+    accept?: string;
+    cancel?: string;
+}
+
+export function Modal(props: IModalProps) {
+
+    return (
+        <div className={"OODCoreModalContainer"}>
+            <div className={"OODCoreModalColumn OODCoreModalMargin"}></div>
+            <div className={"OODCoreModalColumn"}>
+                <div className={"OODCoreModalMargin"}></div>
+                <div className={"OODCoreModalFrame"}>
+                    {props.closable ? <XRow /> : null}
+                    <div className={"OODCoreModalContent"}>
+                        {props.children ? props.children : null}
+                    </div>
+                    <div className={"OODCoreModalButtons"}>
+                        <div className={"OODCoreModalMargin"}></div>
+                        <div className={"OODCoreModalButtonsDiv"}>
+                            {props.accept ? <Button text={props.accept} /> : null}
+                            {props.cancel ? <Button text={props.cancel} /> : null}
+                        </div>
+                    </div>
+                </div>
+                <div className={"OODCoreModalMargin"}></div>
+            </div>
+            <div className={"OODCoreModalColumn OODCoreModalMargin"}></div>
+        </div>
+
+    );
+}
